07c7d0e105b5fc5058ea2fe0837843760316c79a
Rimash94 <Rimhellz94@gmail.com>
2025-12-10 15:07:43 -0600

    Starting point

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    count = num_words(text)
+    print(f"Found {count} total words.")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+def num_words(text):
+    words = text.split()
+    return len(words)    
+
+main()
